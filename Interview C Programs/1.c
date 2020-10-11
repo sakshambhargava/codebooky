103ba57eda2b000b5ea4f17ad9cf2159887c54a3
codebooky <72693037+codebooky@users.noreply.github.com>
2020-10-11 17:20:36 +0530

    Update and rename Interview C Programs.c to Interview C Programs/1.c

---
+++ Interview C Programs/1.c
@@ -0,0 +1,12 @@
+/*C PROGRAMS*/
+
+
+/*1.	Write a C program to print your name, date of birth. and mobile number?*/
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : Alexandra Abramov\n"); 
+     printf("DOB    : July 14, 1975\n"); 
+     printf("Mobile : 99-9999999999\n"); 
+     return(0); 
+  }

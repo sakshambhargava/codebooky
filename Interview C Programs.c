c77d486ac9ab66d979ac755f2b390f38deba84ec
Ishaan Jain <37652866+ishaanjain1898@users.noreply.github.com>
2020-10-11 17:13:38 +0530

    Create Interview C Programs.c

---
+++ Interview C Programs.c
@@ -0,0 +1,25 @@
+/*C PROGRAMS*/
+
+
+/*1.	Write a C program to print your name, date of birth. and mobile number?*/
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : Alexandra Abramov\n"); 
+     printf("DOB    : July 14, 1975\n"); 
+     printf("Mobile : 99-9999999999\n"); 
+     return(0); 
+  }
+/*2.	Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters.*/
+            #include <stdio.h> 
+               int main() 
+              {
+	printf("######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	return(0);
+}
